03147e994f85178d1e11bc4de7081979c2985c9d
Danilo Batista <pereira.b.danilo@gmail.com>
2018-09-28 01:56:17 -0300

    MainPane Modifications

--- src/ui/main/MainController.java
+++ src/ui/main/MainController.java
@@ -2,16 +2,24 @@ package ui.main;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.effects.JFXDepthManager;
 
+import database.DatabaseHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -28,10 +36,32 @@ public class MainController implements Initializable {
     @FXML
     private JFXButton settingsBtn;
     
+    @FXML
+    private JFXTextField bookIdTxt;
+    @FXML
+    private JFXTextField memberIdTxt;
+    
+    @FXML
+    private Text bookNameLabel;
+    @FXML
+    private Text bookAuthorLabel;
+    @FXML
+    private Text bookStatusLabel;
+   
+    @FXML
+    private HBox bookInfo;
+    @FXML
+    private HBox memberInfo;
+    
+    DatabaseHandler databaseHandler;
+
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-				
+		JFXDepthManager.setDepth(bookInfo, 1);
+		JFXDepthManager.setDepth(memberInfo, 1);
+		
+		databaseHandler = DatabaseHandler.getInstance();
 	}
 	
 	private void loadWindow(String location, String title) {
@@ -73,6 +103,32 @@ public class MainController implements Initializable {
     @FXML
     private void loadViewMembers(ActionEvent event) {
     	loadWindow("/ui/memberlist/FXML_MemberList.fxml", "View Members Registered");
+    }
+    
+    @FXML
+    void loadBookInfo(ActionEvent event) {
+    	String id = this.bookIdTxt.getText();
+    	String query = "SELECT * FROM BOOK WHERE id = '" + id + "'; ";
+    	
+    	ResultSet rs = databaseHandler.executeQuery(query);
+    	
+    	try {
+			while (rs.next()) {
+				String name = rs.getString("title");
+				String author = rs.getString("author");
+				Boolean status = rs.getBoolean("isAvail");
+				
+				if (status) {
+					this.bookStatusLabel.setText("DISPONÍVEL");
+				} else {
+					this.bookStatusLabel.setText("NÃO DISPONÍVEL");
+					this.bookStatusLabel.setFill(Paint.valueOf("#ff0000"));
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
 }
